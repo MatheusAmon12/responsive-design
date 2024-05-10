425bbd2beb3a47224e357c38c67bd42381fc1634
Matheus Amon <amonmatheus757@gmail.com>
2024-05-10 15:47:00 -0300

    refactor: add props to container component

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,15 @@ function App() {
   return (
     <TemplateDefault>
       <section className='grid grid-cols-1'>
-        <Container />
+        <Container  src='/Vitejs-logo.svg' alt='react logo' text='ViteJS' />
+
+        <Container  src='/react-original.svg' alt='react logo' text='ReactJS' />
+
+        <Container  src='/nextjs-original.svg' alt='react logo' text='NextJS' />
+
+        <Container  src='/typescript-original.svg' alt='react logo' text='Typescript' />
+
+        <Container  src='/tailwindcss-original-wordmark.svg' alt='react logo' text='Tailwind CSS' />
       </section>
     </TemplateDefault>
   )
--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,8 +1,14 @@
-const Container = () => {
+interface ContainerProps {
+    src: string,
+    alt: string,
+    text: string
+}
+
+const Container = ({ src, alt, text }: ContainerProps) => {
     return ( 
-        <div className="bg-red-500 flex justify-center flex-col items-center py-[53px]">
-            <img src="/nextjs-original.svg" alt="NextJS" className="min-h-16 max-w-52 h-16" />
-            <h2 className="font-serif font-semibold text-[16px]">NextJS</h2>
+        <div className="bg-red-500 flex justify-center flex-col items-center py-[53px] gap-3">
+            <img src={src} alt={alt} className="min-h-16 max-w-52 h-16" />
+            <h2 className="font-serif font-semibold text-[16px]">{text}</h2>
         </div>
      );
 }
