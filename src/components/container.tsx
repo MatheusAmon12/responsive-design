99389d0e00554fed6408f8c4609ec3ed64d8de73
Matheus Amon <amonmatheus757@gmail.com>
2024-05-10 18:50:28 -0300

    fix: ensure text can be show correctly

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -8,10 +8,7 @@ interface ContainerProps {
 const Container = ({ src, alt, text, backgroundGradient }: ContainerProps) => {
     return ( 
         <div 
-            className={`
-                ${backgroundGradient ? backgroundGradient : "text-black text-xs"} 
-                flex justify-center flex-col items-center py-[53px] md:py-28 xl:py-52 gap-3 text-white h-full`
-            }
+            className={`${backgroundGradient ? backgroundGradient : "text-black text-xs"} flex justify-center flex-col items-center py-[53px] md:py-28 xl:py-52 gap-3 text-white h-full`}
         >
             <img src={src} alt={alt} className="h-16 md:h-[88px] xl:h-32 drop-shadow-md" />
             <h2 className="px-4 text-center font-serif font-semibold md:text-lg xl:text-xl">
