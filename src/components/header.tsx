73f6f3388d5a0ba290b0509fed48eac3a86b602d
Matheus Amon <amonmatheus757@gmail.com>
2024-05-10 17:28:35 -0300

    feat: add menu with items in lg screen

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,30 +4,52 @@ import { Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger
 
 const Header = () => {
     return ( 
-        <header className="w-full px-6 flex justify-between items-center pt-6">
-            <h1 className="text-lg font-semibold font-sans">Fusion</h1>
+        <header className="w-full px-6 flex justify-between items-center pt-6 xl:pt-8 ">
+            <h1 className="text-lg font-semibold font-sans md:text-lg xl:text-xl">Fusion</h1>
             <Sheet>
-                <SheetTrigger className="cursor-pointer">
-                    <Button size="icon" variant="ghost">
-                        <Menu />
-                    </Button>
-                </SheetTrigger>
-                <SheetContent className="w-[95vw]">
-                    <SheetHeader>
-                        <SheetTitle className="cursor-pointer text-left pb-4">
-                            Fusion
-                        </SheetTitle>
-                    </SheetHeader>
-                        <div className="font-serif">
-                            <h4>Code Explorer (I'm here!)</h4>
-                            <h4>Code Wizard</h4>
-                            <h4>Code Sensi</h4>
-                        </div>
-                        <SheetFooter className="absolute bottom-4">
-                            Projeto Voluntário
-                        </SheetFooter>
-                </SheetContent>
+                <div className="xl:hidden">
+                    <SheetTrigger className="cursor-pointer">
+                        <Button size="icon" variant="ghost">
+                            <Menu />
+                        </Button>
+                    </SheetTrigger>
+                    <SheetContent className="w-[95vw]">
+                        <SheetHeader>
+                            <SheetTitle className="cursor-pointer text-left pb-4">
+                                Fusion
+                            </SheetTitle>
+                        </SheetHeader>
+                            <div className="font-serif">
+                                <h4>Code Explorer (I'm here!)</h4>
+                                <h4>Code Wizard</h4>
+                                <h4>Code Sensi</h4>
+                            </div>
+                            <SheetFooter className="absolute bottom-4">
+                                Projeto Voluntário
+                            </SheetFooter>
+                    </SheetContent>
+                </div>
             </Sheet>
+
+            <menu className="hidden xl:block">
+                <ul className="xl:flex xl:items-center xl:justify-center xl:gap-4">
+                    <li>
+                        <Button variant="ghost" className="md:text-lg xl:text-xl">
+                            Code Explorer
+                        </Button>
+                    </li>
+                    <li>
+                        <Button variant="ghost" className="md:text-lg xl:text-xl">
+                            Code Wizard
+                        </Button>
+                    </li>
+                    <li>
+                        <Button variant="ghost" className="md:text-lg xl:text-xl">
+                            Code Sensi
+                        </Button>
+                    </li>
+                </ul>
+            </menu>
         </header>
      );
 }
