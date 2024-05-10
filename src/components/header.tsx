ae45e147bbfec79762370444d964386b11a6c152
Matheus Amon <amonmatheus757@gmail.com>
2024-05-10 18:43:21 -0300

    style: apply font size 3xl to logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import { Button } from "./ui/button";
 const Header = () => {
     return ( 
         <header className="w-full px-6 flex justify-between items-center pt-6 pb-2 xl:pt-8 ">
-            <h1 className="text-lg font-semibold font-sans md:text-lg xl:text-xl">Fusion</h1>
+            <h1 className="text-lg font-semibold font-sans md:text-lg xl:text-3xl">Fusion</h1>
 
             <StructuredSheet />
 
