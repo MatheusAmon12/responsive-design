fe222e52ce04ddc1b2280ae444fcc155a978ade7
Matheus Amon <amonmatheus757@gmail.com>
2024-05-10 16:41:30 -0300

    style: add drop shadow to icons

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -8,7 +8,7 @@ interface ContainerProps {
 const Container = ({ src, alt, text, backgroundGradient }: ContainerProps) => {
     return ( 
         <div className={`${backgroundGradient ? backgroundGradient : "text-black text-xs"} flex justify-center flex-col items-center py-[53px] gap-3 text-white`}>
-            <img src={src} alt={alt} className="min-h-16 max-w-52 h-16" />
+            <img src={src} alt={alt} className="min-h-16 max-w-52 h-16 drop-shadow-md" />
             <h2 className="px-4 text-center font-serif font-semibold text-[16px]">{text}</h2>
         </div>
      );
